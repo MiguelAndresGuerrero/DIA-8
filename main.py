157ee4c0eaf04f06ae866d2b04cc72ea66d444c2
Andres Guerrero <166781613+MiguelAndresGuerrero@users.noreply.github.com>
2024-04-18 17:55:36 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #Bienvenida al usuario
 name = input("Ingresa tu nombre ")
 print("Bienvenido ",name)
-chek = input("Ingresa el saldo de tu tarjeta ")
+chek = float(input("Ingresa el saldo de tu tarjeta "))
 
 #Listas donde se guardan la membresia
 Suscripcion1=[]
@@ -20,25 +20,34 @@ OP = int(input("Seleccione la opción: "))
 
 #membresias que se les mostara al usuario
 if OP==1:
-
-    #planes de susucricion que se le mostrataran al usuario
-    if Suscripcion1:
-        print("Esta suscripcion tiene un valor de 20 USD ")
-        TiempoActiva = input("Cuanto tiempo quieres mantener tu suscripcion activa: ")
-    TiempoActiva=Suscripcion1*TiempoActiva
+    #susucricion que se le mostrataran al usuario
+    print("Esta suscripcion tiene un valor de 60 USD por año")
+    TiempoActiva = int(input("Cuánto tiempo quieres mantener tu suscripción activa: "))
+    CostoTotal = 60 * TiempoActiva
+     
+    if chek >= CostoTotal:
+        chek -= CostoTotal
+        Suscripcion1.append(TiempoActiva)
+        print(f"Has comprado la suscripcion por {TiempoActiva} Años")
+        print(f"El costo total para mantener la suscripción activa por {TiempoActiva} meses es: {CostoTotal} USD")
+    else:
+       print("Fondos insuficientes en tu tarjeta")
 
     #Si el usuario desea regalar una suscripcion
-if OP==2:
+elif OP==2:
     name2 = input("A quien desea regalar la cuenta, por favor ingresa el nombre: ")
-    input("Cuanto tiempo deseas regalarle la suscripcion ",TiempoActiva)
-    chek=chek-TiempoActiva
-    print("tu amigo",name2), print("Activo su suscripcion gracias a ",name)
-
+    input("Cuanto tiempo deseas regalarle la suscripcion ")
+    MembrasiaRegalada=int(input(f"Cuantos años desea regalarle la suscripcion a {name2}: "))
+    
+    #verificamos que el usuario si tenga fondos suficientes
+    if chek >=MembrasiaRegalada*60:
+        chek -=MembrasiaRegalada*60
+        print("tu amigo",name2), print("Activo su suscripcion gracias a ",name)
     #Si el usuario desea verificar el saldo que le queda en la tarjeta
-if OP==3:
-    chek=chek-TiempoActiva
-    print("chek")
+elif OP==3:
+    print(f"El saldo actual de tu tarjeta es: {chek} USD")
 
+#le agradecemos al usuario por comprar la suscripcion
 print("Gracias por comprar la suscripcion ", name)
 
 #creado por Miguel Guerrero C.C 1090381839
